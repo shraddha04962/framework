6fca75681e77dec5bc740d556aca3ed7f476bcf8
shraddha04962 <sbd0496@gmail.com>
2023-09-13 20:26:44 -0700

    added one line for demo

--- src/test/java/TestCases/LoginTestCase.java
+++ src/test/java/TestCases/LoginTestCase.java
@@ -14,6 +14,7 @@ import Resources.Constants;
 
 public class LoginTestCase extends BaseClass {
 
+	//added one line for git demo purpose
 	@Test(priority = 0)
 
 	public void verifyingValidLogin() throws IOException, InterruptedException {
